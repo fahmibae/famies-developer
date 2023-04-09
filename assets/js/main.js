c3df4d8e2b63c63e804abc1cb4e631e24513c3cc
fahmibae <58241123+fahmibae@users.noreply.github.com>
2023-04-10 06:25:18 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,40 @@ $(document).ready(function ($) {
       document.execCommand("copy");
       document.getElementById("PesanCopy").innerHTML = msg + '</p>';
    }
+  
+   var swiper = new Swiper(".slides-1", {
+            pagination: {
+                el: ".swiper-pagination",
+            },
+        });
+new Swiper('.slides-3', {
+            speed: 600,
+            loop: true,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false
+            },
+            slidesPerView: 'auto',
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 40
+                },
+
+                1200: {
+                    slidesPerView: 3,
+                }
+            }
+        });
 
 
   /**
